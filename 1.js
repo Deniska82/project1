01881c7f6796dfd38718f750e45c97f271ec17bf
Denis <tone108@yandex.ru>
2020-12-03 21:13:22 +0300

    first commit

---
+++ 1.js
@@ -0,0 +1,8 @@
+function pow2() 
+    //document.getElementById('inp1').value
+let num1;
+num1 = document.getElementById('inp1').value;
+    alert (num1*num1);
+    num1 = 7;
+    var  b=5;
+    alert(num1 + b);
---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6 */
+/* jslint node: true */
+/* jshint browser: true */
+"use strict";
+
+console.log('arr' + "-object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8');
+ 
+const isChecket = false,
+      isClose = false;
+
+console.log(isChecket || !isClose);      
+
