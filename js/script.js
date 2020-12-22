dfdda6913ea319a334070859d599ab213448e54d
Denis <tone108@yandex.ru>
2020-12-22 20:27:37 +0300

    22.12.2020

--- js/script.js
+++ js/script.js
@@ -3,22 +3,21 @@
 /* jshint browser: true */
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
 
-const personalMovieDB  = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false 
-};
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько его отцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько его отцените?', '');
+const num = 50;
 
-personalMovieDB.movies[a] = b;      
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+switch (num) {
+   case 49:
+      console.log('Неверно');
+      break;
+   case 100:
+      console.log('Неверно');
+      break;
+   case 50:
+      console.log('Верно');
+      break;
+   default:
+      console.log('Не в этот раз!');
+      break;
+}
