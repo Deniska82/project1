ec2ef08a235fcd2e73c91b22ecf5c7016533a731
Denis <tone108@yandex.ru>
2020-12-23 21:07:38 +0300

    cycle

--- js/script.js
+++ js/script.js
@@ -5,19 +5,23 @@
 
 
 
-const num = 50;
+let num = 50;
 
-switch (num) {
-   case 49:
-      console.log('Неверно');
-      break;
-   case 100:
-      console.log('Неверно');
-      break;
-   case 50:
-      console.log('Верно');
-      break;
-   default:
-      console.log('Не в этот раз!');
-      break;
+// while (num <= 55) {
+//    console.log(num);
+//    num++;
+// }
+
+// do {
+//    console.log(num);
+//    num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+   if (i ===6 ) {
+      continue;
+   }
+   console.log(i);
+   
 }
