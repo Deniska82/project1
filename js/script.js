d591ce49a120debe5328a992b2718ee9699d69be
Denis <tone108@yandex.ru>
2020-12-29 21:08:39 +0300

    function

--- js/script.js
+++ js/script.js
@@ -3,42 +3,35 @@
 /* jshint browser: true */
 "use strict";
 
+let num = 20;
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+} 
+showFirstMessage("Hello world!");
+console.log(num);
 
 
-for (let i = 0; i < 2; i++) {
-   const a = prompt('Один из последних просмотренных фильмов?', ''),
-         b = prompt('На сколько оцените его?', '');
 
-         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-         } else {
-             console.log('error');
-             i--;
-         }
-         if (personalMovieDB.count < 10) {
-             console.log("Просмотрено довольно мало фильмов");
-         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log("Вы классический зритель");
-         } else if (personalMovieDB.count >= 30) {
-             console.log("Вы киноман");
-         } else {
-             console.log("Произошла ошибка");
-         }
-   
+function ret() {
+    let num = 50;
+
+    return num;
 }
 
-console.log(personalMovieDB); 
+const anotherNum = ret();
+console.log(anotherNum);
 
+const logger = function() {
+     console.log("Hello");
+};
+
+logger();
+
+const calc = (a, b) => {
+    console.log('1');
+    return a + b;
+};
 
