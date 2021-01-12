a92a1876b0b2cf0736646c3d0424d0817291ab67
Denis <tone108@yandex.ru>
2021-01-12 21:21:52 +0300

    callback

--- js/script.js
+++ js/script.js
@@ -4,71 +4,28 @@
 
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-} 
-
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
+function first() {
+    // Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
 }
 
-//rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
+function second() {
+    console.log(2);
 }
 
-//detectPersonalLevel();
+first();
+second();
 
-function showMyDB (hidden) {
-    if(!hidden) {
-        console.log(personalMovieDB);
-    }
+
+function learnJS(land, callback) {
+    console.log(`Я учу: ${land}`);
+    callback();
 }
-showMyDB(personalMovieDB);
 
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-         personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
+function done(){
+    console.log('Я прошел этот урок!');
 }
-writeYourGenres();
 
-
+learnJS('JavaScript', done);
