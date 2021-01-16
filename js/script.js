16f702e579ed7c0531572bff4fb9e0a720a96f18
Denis <tone108@yandex.ru>
2021-01-16 20:54:07 +0300

    object

--- js/script.js
+++ js/script.js
@@ -4,28 +4,43 @@
 
 'use strict';
 
-function first() {
-    // Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-first();
-second();
+const {border, bg} = options.colors;
+console.log(border);
 
+//console.log(Object.keys(options).length);
 
-function learnJS(land, callback) {
-    console.log(`Я учу: ${land}`);
-    callback();
-}
+ //console.log(options["colors"]["border"]);
 
-function done(){
-    console.log('Я прошел этот урок!');
-}
+// delete options.name;
 
-learnJS('JavaScript', done);
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
