855a813cb757a10e26e12854aa65adee5d8b5b9f
Denis <tone108@yandex.ru>
2021-02-03 20:54:36 +0300

    oop

--- js/script.js
+++ js/script.js
@@ -4,98 +4,32 @@
 
 'use strict';
 
-// let a = 5,
-//     b = a;
+let str = "some";
+let strObj = new String(str);
 
-// b = b + 5;
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
-// console.log(b);
-// console.log(a);
+console.dir([1, 2, 3]);
 
-// const obj = {
-//     a: 5,
-//     b: 1
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
+    }
+};
+
+const jonh = Object.create(soldier);
+
+// const jonh = {
+//     health: 100
 // };
 
-// const copy = obj;//Ссылка
+// jonh.__proto__ = soldier;
 
-// copy.a = 10;
+// Object.setPrototypeOf(jonh, soldier);
 
-// console.log(copy);
-// console.log(obj);
+// console.log(jonh.armor);
 
-function copy(mainObj) {
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
-};
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-// console.log(newNumbers);
-// console.log(numbers);
-
-const add = {
-    d: 17,
-    e: 20
-};
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'djjfkdjfj';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejourna', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ["a", "b",];
-
-const newAarray = [...array];
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const newObj = {...q};
-
-console.log(newObj);
+ jonh.sayHello();
