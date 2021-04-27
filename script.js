013b26ab5dc318188fa5d17691753957ffbc6b36
Denis <tone108@yandex.ru>
2021-04-27 20:56:17 +0300

    new open

---
+++ script.js
@@ -0,0 +1,22 @@
+/*jshint esversion: 6 */
+/* jslint node: true */
+/* jshint browser: true */
+
+//console.log(document.head);
+//console.log(document.documentElement);
+// console.log(document.body.childNodes);
+//console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+//console.log(document.body.lastChild);
+
+//console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentElement);
+
+//console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
+}
